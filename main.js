21e9aaa54e3733a98a82767bfd969197ea9f1936
Ryan Manchester <ryanm17@gmail.com>
2021-08-28 17:26:57 -0400

    basic threejs setup

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-import * as THREE from 'three';
+const THREE = require('three');
 
 const scene = new THREE.Scene();
-const camera = 
+const camera = new THREE.PerspectieCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
