e01491cdeaf8b583180fc46e796635686d10c32e
Ryan Manchester <ryanm17@gmail.com>
2021-08-29 16:43:19 -0400

    renders basic green square

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-
+//Boilerplate THREE setup
+//1. Initialize the scene and PerspectiveCamera
+//2. Initialize renderer and set the size
+//3. Append the renderer to the browser body
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -10,3 +13,18 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#e5e5e5");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+//Create a floating cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+//Create an animate loop to render cube to Scene
+const animate = () => {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera);
+}
+
+animate();
