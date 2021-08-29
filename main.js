ac9102774078bbc70fbe171018bd5b4e1916b81e
Ryan Manchester <ryanm17@gmail.com>
2021-08-29 16:30:42 -0400

    initial renderer working

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const THREE = require('three');
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectieCamera(
+const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("#e5e5e5");
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
